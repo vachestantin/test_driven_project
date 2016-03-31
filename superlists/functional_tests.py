67adc1b13edabd08e592167931b333464ee8068f
vachestantin <vachestantin@gmail.com>
2016-03-31 23:12:01 +0900

    템플릿을 만들고 홈페이지 뷰를 리펙터링

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,10 +1,11 @@
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
 class NewVisitorTest(unittest.TestCase):
-    
+
     def setUp(self): #각각의 테스트를 수행하지 전에 브라우저를 실행한다
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3) #그냥 3초간 기다리는 것
@@ -13,10 +14,32 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self): #테스트 내용을 알 수 있는 명칭으로 정하는 것이 좋다
+        # 에디스는 멋진 온라인 앱이 나왔다는 소식을 듣고 해당 웹 사이트를 확인하러 간다
         self.browser.get('http://localhost:8000')
 
+        #웹 페이지 타이틀과 헤더가 'To-Do'를 표시하고 있다
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!') #강제적으로 테스트 실패를 밣생시켜 에러 메시지를 출력한다
+        header_text = self.browser.find_elements_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            '작업 아이템 입력'
+        )
+
+        inputbox.send_keys('공작깃털 사기')
+
+        inputbox.send_keys('Keys.ENTER')
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: 공작깃털 사기' for row in rows),
+        )
+
+        #강제적으로 테스트 실패를 밣생시켜 에러 메시지를 출력한다
+        self.fail('Finish the test!')
 
         if __name__ == '__main__':
             unittest.main(warnings='ignore')
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,8 +1,8 @@
-# import unittest
 
 from django.core.urlresolvers import resolve
-from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
+from django.test import TestCase
 
 from lists.views import home_page
 
@@ -16,6 +16,8 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html) # decode를 이용해서 바이트 데이터를 유니코드 문자열로 변환한다
+        # self.assertTrue(response.content.startswith(b'<html>'))
+        # self.assertIn(b'<title>To-Do lists</title>', response.content)
+        # self.assertTrue(response.content.strip().endswith(b'</html>')) # strip은 html파일 마지막 부분에 있는 빈 공간의 문제를 예방해준다
