1d590fde585b9914c6d51d082b4b3ffb4fd50cb3
vachestantin <vachestantin@gmail.com>
2016-04-01 00:21:07 +0900

    csrf token 때문에 계속 테스트 실패..

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,3 +1,4 @@
+import unittest
 
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
@@ -13,8 +14,24 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
+    # @unittest.skip
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html) # decode를 이용해서 바이트 데이터를 유니코드 문자열로 변환한다
+        # decode를 이용해서 바이트 데이터를 유니코드 문자열로 변환한다
+        self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+
+        response = home_page(request)
+
+        self.assertIn('신규 작업 아이템', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': '신규 작업 아이템'}
+        )
+        self.assertEqual(response.content.decode(), expected_html)
