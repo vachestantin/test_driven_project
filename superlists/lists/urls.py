686818fb246824b1e82ae7c4f2677ff57712e729
vachestantin <vachestantin@gmail.com>
2016-04-05 14:23:49 +0900

    URL include를 이용한 리팩터링 성공 lists/urls.py 앱의 url을 분리 시킴

---
+++ superlists/lists/urls.py
@@ -0,0 +1,12 @@
+
+from django.conf.urls import patterns, url
+from . import views as lists_views
+
+# app_name = 'lists'
+
+urlpatterns = [
+    url(r'^(?P<pk>[0-9]+)/$', lists_views.view_list, name='view_list'),
+    url(r'^(?P<pk>[0-9]+)/add_item$', lists_views.add_item, name='add_item'),
+    url(r'^new$', lists_views.new_list, name='new_list'),
+
+]
--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -3,12 +3,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from lists import views as lists_views
+from lists.urls import urlpatterns as lists_urls
 
 urlpatterns = [
     url(r'^$', lists_views.home_page, name='home'),
-    url(r'^lists/(?P<pk>[0-9]+)/$', lists_views.view_list, name='view_list'),
-    url(r'^lists/(?P<pk>[0-9]+)/add_item$', lists_views.add_item, name='add_item'),
-    url(r'^lists/new$', lists_views.new_list, name='new_list'),
+    url(r'^lists/', include(lists_urls, namespace='lists')),
 
-    # url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
 ]
