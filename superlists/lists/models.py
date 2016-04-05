4bd634b9f02ffa38ae008672d072e3de467d9993
vachestantin <vachestantin@gmail.com>
2016-04-05 14:41:28 +0900

    간단하게 admin 페이지 생성

--- superlists/lists/models.py
+++ superlists/lists/models.py
@@ -4,6 +4,13 @@ from django.db import models
 class List(models.Model):
     pass
 
+    def __str__(self):
+        return '{}번째 리스트'.format(self.pk)
+
+
 class Item(models.Model):
     text = models.TextField(default='')
     list = models.ForeignKey(List, default=None)
+
+    def __str__(self):
+        return '{}번째 아이템: {}'.format(self.pk, self.text)
