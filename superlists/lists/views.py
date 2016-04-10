716b29ef21ac3398ad76c19a53dce4fd50d59d20
vachestantin <vachestantin@gmail.com>
2016-04-10 23:36:33 +0900

    리스트 뷰에서 모델 유효성 검증 구현

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -8,15 +8,6 @@ from lists.models import Item, List
 def home_page(request):
     return render(request, 'home.html')
 
-def view_list(request, pk):
-    list_ = List.objects.get(id=pk)
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'], list=list_)
-        return redirect('/lists/%d/' % (list_.id,))
-    return render(request, 'list.html', {
-        'list': list_,
-    })
-
 def new_list(request):
     list_ = List.objects.create()
     item = Item.objects.create(text=request.POST['item_text'], list=list_)
@@ -29,7 +20,26 @@ def new_list(request):
         return render(request, 'home.html', {"error": error})
     return redirect('/lists/%d/' % (list_.id,))
 
-def add_item(request, pk):
+def view_list(request, pk):
     list_ = List.objects.get(id=pk)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/%d/' % (list_.id,))
+    error = None
+
+    if request.method == 'POST':
+        try:
+            item = Item(text=request.POST['item_text'], list=list_)
+            item.full_clean()
+            item.save()
+            return redirect('/lists/%d/' % (list_.id,))
+        except ValidationError:
+            error = "You can't have an empty list item"
+
+    return render(request, 'list.html', {
+        'list': list_,
+        'error': error,
+    })
+
+
+# def add_item(request, pk):
+#     list_ = List.objects.get(id=pk)
+#     Item.objects.create(text=request.POST['item_text'], list=list_)
+#     return redirect('/lists/%d/' % (list_.id,))
