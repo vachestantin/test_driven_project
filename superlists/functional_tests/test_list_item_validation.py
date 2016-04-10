4534de4f7505c2e3971bcf85393a592922684cb1
vachestantin <vachestantin@gmail.com>
2016-04-10 21:34:23 +0900

    모델 검증을 위한 신규 목록 뷰 수정

--- superlists/functional_tests/test_list_item_validation.py
+++ superlists/functional_tests/test_list_item_validation.py
@@ -1,6 +1,8 @@
 
 from .base import FunctionalTest
 
+# from selenium.webdriver.common.keys import Keys
+
 from unittest import skip
 
 
@@ -13,23 +15,21 @@ class ItemValidationTest(FunctionalTest):
 
         # 에러 메시지가 표시된다
         error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text, "빈 아이템을 등록할 수 없습니다")
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         # 다른 아이템을 입력하고 이번에는 정상 처리된다
-        self.browser.find_element_by_id('id_new_item')/send_keys('우유 사기\n')
+        self.browser.find_element_by_id('id_new_item').send_keys('우유 사기\n')
         self.check_for_row_in_list_table('1: 우유 사기')
 
         # 그녀는 고의적으로 다시 빈 아이템을 등록한다
-        self.browser.find_element_by_id('id_new_item')/send_keys('\n')
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         # 리스트 페이지에 다시 에러 메시지가 표시된다
         self.check_for_row_in_list_table('1: 우유 사기')
         error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text, "빈 아이템을 등록할 수 없습니다")
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         # 아이템을 입력하면 정상동작한다
-        self.browser.find_element_by_id('id_new_item')/send_keys('차 만들기\n')
+        self.browser.find_element_by_id('id_new_item').send_keys('차 만들기\n')
         self.check_for_row_in_list_table('1: 우유 사기')
         self.check_for_row_in_list_table('2: 차 만들기')
-
-        # self.fail('wirte me!')
