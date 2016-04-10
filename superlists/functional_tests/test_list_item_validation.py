eaf2167551f4961079318975f53a48734daddf45
vachestantin <vachestantin@gmail.com>
2016-04-10 19:53:12 +0900

    단위 테스트 분리

--- superlists/functional_tests/test_list_item_validation.py
+++ superlists/functional_tests/test_list_item_validation.py
@@ -5,17 +5,31 @@ from unittest import skip
 
 
 class ItemValidationTest(FunctionalTest):
-    
+
     def test_cannot_add_empty_list_items(self):
         # 입력 상자가 비어 있는 상테에서 엔터키를 누른다
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         # 에러 메시지가 표시된다
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "빈 아이템을 등록할 수 없습니다")
 
         # 다른 아이템을 입력하고 이번에는 정상 처리된다
+        self.browser.find_element_by_id('id_new_item')/send_keys('우유 사기\n')
+        self.check_for_row_in_list_table('1: 우유 사기')
 
         # 그녀는 고의적으로 다시 빈 아이템을 등록한다
+        self.browser.find_element_by_id('id_new_item')/send_keys('\n')
 
         # 리스트 페이지에 다시 에러 메시지가 표시된다
+        self.check_for_row_in_list_table('1: 우유 사기')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "빈 아이템을 등록할 수 없습니다")
 
         # 아이템을 입력하면 정상동작한다
-        self.fail('wirte me!')
+        self.browser.find_element_by_id('id_new_item')/send_keys('차 만들기\n')
+        self.check_for_row_in_list_table('1: 우유 사기')
+        self.check_for_row_in_list_table('2: 차 만들기')
+
+        # self.fail('wirte me!')
--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -28,30 +28,6 @@ class HomePageTest(TestCase):
         self.assertEqual(observed_html, expected_html)
 
 
-class ItemModelTest(TestCase):
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = '첫 번째 아이템'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = '두 번째 아이템'
-        second_item.list = list_
-        second_item.save()
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, '첫 번째 아이템')
-        self.assertEqual(second_saved_item.text, '두 번째 아이템')
-
-
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
         list_ = List.objects.create()
