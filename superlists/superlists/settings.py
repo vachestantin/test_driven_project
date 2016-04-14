676c7df7961d145aa1f753341f2e08befacf5052
vachestantin <vachestantin@gmail.com>
2016-04-14 18:43:04 +0900

    persona를 이용한 사용자 정의 인증 처리 스파이크 로깅 코드 추가

--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -116,3 +116,20 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static')) # 얘는 그냥 하나, 리스트나 튜플 아님
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
